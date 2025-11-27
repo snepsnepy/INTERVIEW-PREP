17ff4f157ca2204830a505e850a7caa53c5f60f4
Savulescu Mihnea-Alexandru <snepysneptest@gmail.com>
2025-11-28 01:24:42 +0200

    Time: 47 ms (81.76%), Space: 58.8 MB (5.62%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    const seen = new Set();
+
+    while(head) {
+        if (seen.has(head)) {
+            return head;
+        }
+        seen.add(head);
+        head = head.next;
+    }
+
+    return null;
+};
