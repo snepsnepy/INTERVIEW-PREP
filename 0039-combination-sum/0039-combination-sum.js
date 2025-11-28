5a868969406e784ec7f79be5a501897a8d740153
Savulescu Mihnea-Alexandru <snepysneptest@gmail.com>
2025-11-28 16:01:02 +0200

    Time: 3 ms (58.58%), Space: 58.9 MB (83.54%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let res = [];
+
+    const dfs = (i, cur, total) => {
+        if (total === target) {
+            res.push([...cur]);
+            return;
+        }
+
+        if (i >= candidates.length || total > target) return;
+
+        cur.push(candidates[i]);
+        dfs(i, cur, total + candidates[i]);
+        cur.pop();
+        dfs(i + 1, cur, total)
+    }
+
+    dfs(0, [], 0);
+
+    return res;
+};
