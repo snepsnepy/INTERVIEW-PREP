9e58e5610527d73510a429258325e6fbc786f9db
Savulescu Mihnea-Alexandru <snepysneptest@gmail.com>
2025-11-30 20:35:29 +0200

    Time: 24 ms (49.08%), Space: 61.2 MB (27.05%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const getDrome = (left, right, string) => {
+    while (left >= 0 && right < string.length) {
+        if (string[left] !== string[right]) break;
+        left--;
+        right++;
+    }
+
+    return [left + 1, right];
+}
+
+function longestPalindrome(s) {
+    let max = [0, 1];
+
+    for (let i = 0; i < s.length; i++) {
+        let even = getDrome(i - 1, i, s);
+        let odd = getDrome(i - 1, i + 1, s);
+
+        let curMax = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+
+        max = max[1] - max[0] > curMax[1] - curMax[0] ? max : curMax;
+    }
+
+    return s.slice(max[0], max[1]);
+}
