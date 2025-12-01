92874110a7452f8aabe6c30ea17c3d6d735dc1c6
Savulescu Mihnea-Alexandru <snepysneptest@gmail.com>
2025-12-02 00:52:47 +0200

    Time: 0 ms (100%), Space: 55.3 MB (79.89%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if (!root) return [];
+    let res = [];
+    let queue = [root];
+
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift();
+            if (i === levelSize - 1) {
+                res.push(node.val);
+            }
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+
+    return res;
+};
